bbec1c04af1c6146a34e50d70870d163b43ffefb
dkMaster3000 <kunzd007@gmail.com>
2024-06-25 17:58:47 +0200

    created undead models creator

--- src/main/java/main/ModelsView.java
+++ src/main/java/main/ModelsView.java
@@ -1,17 +1,21 @@
 package main;
 
 import main.models.uicomponents.ModelPanel;
+import main.models.uicomponents.UndeadCreator;
 import main.models.undead.Undead;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ModelsView extends JPanel {
+public class ModelsView extends JPanel implements ActionListener {
 
-    List<Undead> undeads = new ArrayList<>();
+    private final List<Undead> undeads = new ArrayList<>();
+
     final Undead zombie = new Undead("Zombie",
             10, 0, "1d4 Pro Einheit + STK des Stacks",
             1, 0, 0,
@@ -28,6 +32,26 @@ public class ModelsView extends JPanel {
             10, 0, "Seelenentzug: Fügt dem Ziel 1d20+Wissen des Stacks Schaden zu, 7 Range",
             0, 0, 2,
             "Das Ziel ist markiert und die Seele des Ziels wird als Banshee dem Stack nach dem Tod hinzugefügt", false);
+    final Undead reaper = new Undead("Sensenmann",
+            80, 15, "1d12 + GES des Stacks",
+            0, 4, 0,
+            "Ermöglicht fliegen, Angriffe fügen 15% des gewürfelten Schadens als zusätzlichen True Damage Schaden zu, Getötete Ziele werden zu Banshees",
+            true);
+    final Undead lich = new Undead("Lich",
+            80, 15, "Fernangriff: Feuert einen nekrotischen Strahl welcher 1d20+Wissen des Stacks Schaden verursacht und die Rüstung des Ziel um 1d20 senkt",
+            0, 0, 4,
+            "Kann Zombies oder Skelette dem Stack hinzufügen",
+            true);
+    final Undead monstro = new Undead("Monstrosität",
+            120, 20, "1d20+Stärke des Stacks",
+            6, 0, 0,
+            "Feinde in der Nähe der Monstrosität werden infiziert, infizierte Feinde erhalten zu Beginn jeder Runde 5% MAX HP Schaden, der Stack erhält DR und macht % mehr Schaden in Höhe der Gesamteinheiten im Stack(Max 30%)",
+            true);
+    final Undead boneDragon = new Undead("Knochendrache",
+            120, 20, "1d20+STK+INT+Ges des Stacks",
+            2, 2, 2,
+            "Ermöglicht fliegen, Angriffe verlangsamen Gegner um 1, Getötete Feinde werden als Skelettkrieger oder Skelettschütze dem Stack hinzugefügt, Stackeigenschaften von Skeletten werde verdoppelt, Schaden wird mit jeder Einheit im Stack um 2% erhöht (Max 50)",
+            true);
 
     public ModelsView() {
         setBorder(new EmptyBorder(20, 20, 20, 20));
@@ -37,6 +61,17 @@ public class ModelsView extends JPanel {
         undeads.add(skelettalWarrior);
         undeads.add(skelettalArcher);
         undeads.add(banshee);
+        undeads.add(reaper);
+        undeads.add(lich);
+        undeads.add(monstro);
+        undeads.add(boneDragon);
+
+        updateModelsView();
+
+    }
+
+    private void updateModelsView() {
+        removeAll();
 
         for (Undead undead : undeads) {
             ModelPanel newModelPanel = new ModelPanel(undead);
@@ -45,7 +80,21 @@ public class ModelsView extends JPanel {
         }
 
         JButton addButton = new JButton("+");
+        addButton.addActionListener(this);
         add(addButton);
 
+        revalidate();
+        repaint();
+    }
+
+    private void addNewUndead(Undead newUndead) {
+        undeads.add(newUndead);
+
+        updateModelsView();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        new UndeadCreator(this::addNewUndead);
     }
 }
---
+++ src/main/java/main/models/uicomponents/UndeadCreator.java
@@ -0,0 +1,149 @@
+package main.models.uicomponents;
+
+import main.models.undead.Undead;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.util.function.Consumer;
+
+public class UndeadCreator extends JFrame implements ActionListener {
+
+    Consumer<Undead> addToModelsView;
+    JTextField nameTF, hpTF, armorTF, dmgTF, strengthTF, dexterityTF, intelligenceTF, characteristicsTF;
+    JComboBox<String> leaderCB;
+    JLabel notificationLabel;
+
+    private final String YES_CHOICE = "Ja";
+
+    public UndeadCreator(Consumer<Undead> addToModelsView) {
+        this.addToModelsView = addToModelsView;
+
+        setTitle("Undead Creator");
+        setSize(500, 800);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        JPanel container = new JPanel();
+        container.setBackground(Color.cyan);
+        container.setBorder(new EmptyBorder(20, 20, 20, 20));
+        container.setLayout(new GridLayout(0, 2, 20, 20));
+
+        JLabel nameLabel = new JLabel("Name(Text):");
+        container.add(nameLabel);
+
+        String DEFAULT_NUMBER_VALUE = "0";
+
+        nameTF = new JTextField();
+        container.add(nameTF);
+
+        JLabel hpLabel = new JLabel("HP(Zahl):");
+        container.add(hpLabel);
+
+        hpTF = new JTextField("10");
+        container.add(hpTF);
+
+        JLabel armorLabel = new JLabel("Rüstung(Zahl):");
+        container.add(armorLabel);
+
+
+        armorTF = new JTextField(DEFAULT_NUMBER_VALUE);
+        container.add(armorTF);
+
+        JLabel dmgLabel = new JLabel("DMG(Text):");
+        container.add(dmgLabel);
+
+        dmgTF = new JTextField();
+        container.add(dmgTF);
+
+        JLabel strengthLabel = new JLabel("STK(Zahl):");
+        container.add(strengthLabel);
+
+        strengthTF = new JTextField(DEFAULT_NUMBER_VALUE);
+        container.add(strengthTF);
+
+        JLabel dexterityLabel = new JLabel("GES(Zahl):");
+        container.add(dexterityLabel);
+
+        dexterityTF = new JTextField(DEFAULT_NUMBER_VALUE);
+        container.add(dexterityTF);
+
+        JLabel intelligenceLabel = new JLabel("INT(Zahl):");
+        container.add(intelligenceLabel);
+
+        intelligenceTF = new JTextField(DEFAULT_NUMBER_VALUE);
+        container.add(intelligenceTF);
+
+        JLabel characteristcLabel = new JLabel("Eigenschaft(Text):");
+        container.add(characteristcLabel);
+
+        characteristicsTF = new JTextField();
+        container.add(characteristicsTF);
+
+        JLabel leaderLabel = new JLabel("Ist Anführer?:");
+        container.add(leaderLabel);
+
+        leaderCB = new JComboBox<>(new String[]{YES_CHOICE, "Nein"});
+        container.add(leaderCB);
+
+
+        JButton saveButton = new JButton("Speichern");
+        saveButton.addActionListener(this);
+        container.add(saveButton);
+
+        notificationLabel = new JLabel("Werte sind Falsch!");
+        notificationLabel.setForeground(Color.RED);
+        notificationLabel.setVisible(false);
+        container.add(notificationLabel);
+
+        add(container);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (valuesAreUseable()) {
+            addToModelsView.accept(
+                    new Undead(nameTF.getText(),
+                            Integer.parseInt(hpTF.getText()),
+                            Integer.parseInt(armorTF.getText()),
+                            dmgTF.getText(),
+                            Integer.parseInt(strengthTF.getText()),
+                            Integer.parseInt(dexterityTF.getText()),
+                            Integer.parseInt(intelligenceTF.getText()),
+                            characteristicsTF.getText(),
+                            isLeader()));
+
+            dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+        } else {
+            notificationLabel.setVisible(true);
+        }
+
+    }
+
+    private boolean valuesAreUseable() {
+        if (nameTF.getText().isEmpty()) {
+            return false;
+        }
+
+        try {
+            Integer.parseInt(hpTF.getText());
+            Integer.parseInt(armorTF.getText());
+            Integer.parseInt(strengthTF.getText());
+            Integer.parseInt(dexterityTF.getText());
+            Integer.parseInt(intelligenceTF.getText());
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isLeader() {
+        return leaderCB.getSelectedItem() == YES_CHOICE;
+    }
+}
