33fe2a01ed9c8138a395a47cf13ab7688d5152ed
dkMaster3000 <kunzd007@gmail.com>
2024-07-02 15:45:19 +0200

    set a theme

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Set FlatLaf look and feel
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace(System.err);
+
+        }
         new MainFrame();
     }
 }
